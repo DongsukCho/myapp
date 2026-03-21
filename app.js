fe6b8ae24009137e917f53a1b483373cf2aeb0d0
Dongsuk Cho <gensbomb@gmail.com>
2026-03-21 22:09:59 +0900

    monitoring

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const client = require('prom-client');
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js on Kubernetes 🚀');
+const app = express();
+const port = 3000;
+
+// 기본 메트릭 수집
+client.collectDefaultMetrics();
+
+// 커스텀 메트릭
+const counter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of requests'
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.get('/', (req, res) => {
+  counter.inc();
+  res.send('Hello DevOps!');
+});
+
+// Prometheus metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
